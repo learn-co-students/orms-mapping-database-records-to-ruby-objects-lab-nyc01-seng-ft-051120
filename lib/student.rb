8609a6dd02e25c53b06c8221d7357ce3da13a3c1
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-21 15:34:39 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,79 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    search_student_db_no_args(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    search_student_db_no_args(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    search_student_db_no_args(sql)
+  end
+
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    search_student_db_one_arg(sql, number)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    search_student_db_one_arg(sql, grade)
+  end
+
+
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1).first
+  end
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    search_student_db_one_arg(sql, name).first
   end
   
   def save
@@ -40,4 +103,19 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  private
+
+  def self.search_student_db_no_args(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.search_student_db_one_arg(sql, arg)
+    DB[:conn].execute(sql, arg).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
